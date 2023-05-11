d0bc3629d84441e1335c91416b43ffbb4d24a92e
Touseef Ahmed <touseefahmed1214@gmail.com>
2023-05-11 21:31:59 +0100

    remaining sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
                 'black': '#000000',
                 'light-yellow': '#F8F3E4',
                 'dark-yellow': '#F5A54F',
-                'light-yellow': '#838383',
+                'light-gray': '#6A6A6A',
+                'light-silver': '#838383',
+                
+
             },
             container: {
                 screens:{
@@ -24,18 +27,20 @@ module.exports = {
                 'space-mono': ['Space Mono', 'monospace'],
               },
             fontSize: {
-                xsm: '0.75rem',
+                xsm: '0.75rem',//12
                 sm: '0.813rem',//13px
                 base: '0.875rem',
-                xl: '1.25rem',
+                xl: '1.25rem',//20px
                 '2xl': '1.313rem',//21px
                 '3xl':'1.375rem',
+                '4xl':'3rem',//48px
               },
               lineHeight: {
                 '34': '34px',
                 '18': '18px',
                 '19': '19px',
                 '17': '17px',
+                '13': '13px',
               }
         },
     },
