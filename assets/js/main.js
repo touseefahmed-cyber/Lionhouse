254c77e306cce2afbdce4ce4317318c0a46ca300
Touseef Ahmed <touseefahmed1214@gmail.com>
2023-05-12 21:52:05 +0100

    Footer and slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,47 @@
-import '../css/style.css'
+import '../css/style.css';
+
+import BlazeSlider from 'blaze-slider';
+import 'blaze-slider/dist/blaze.css';
+
+(function() {
+
+
+const ell = document.querySelector('#blaze-slider2');
+new BlazeSlider(ell, {
+  all: {
+    autoplayDirection: 'to Right',
+    enableAutoplay: true,
+    autoplayInterval: 2000,
+    transitionDuration: 300,
+    slidesToShow: 6,
+  },
+  '(max-width: 900px)': {
+    slidesToShow: 2,
+  },
+  '(max-width: 500px)': {
+    slidesToShow: 6,
+  },
+})
+
+
+
+const el = document.querySelector('#blaze-slider1');
+new BlazeSlider(el, {
+  all: {
+    autoplayDirection: 'to Right',
+    enableAutoplay: true,
+    autoplayInterval: 2000,
+    transitionDuration: 300,
+    slidesToShow: 3,
+  },
+  '(max-width: 900px)': {
+    slidesToShow: 2,
+  },
+  '(max-width: 500px)': {
+    slidesToShow: 1,
+  },
+});
+
+
+
+})();
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,13 +27,18 @@ module.exports = {
                 'space-mono': ['Space Mono', 'monospace'],
               },
             fontSize: {
+                xxsm: '0.625rem',//12
                 xsm: '0.75rem',//12
                 sm: '0.813rem',//13px
-                base: '0.875rem',
+                base: '0.875rem',//14px
                 xl: '1.25rem',//20px
                 '2xl': '1.313rem',//21px
                 '3xl':'1.375rem',//22px
                 '4xl':'3rem',//48px
+                '5xl':'7.75rem',//124
+                '6xl':'9.375rem',//124
+                '7xl':'11.625rem',//186
+                '8xl':'16.375rem',//262px
               },
               lineHeight: {
                 '34': '34px',
