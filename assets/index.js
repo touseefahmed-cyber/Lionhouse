37e2e37c9aed557749fca1158b45b4433bbeb96f
Touseef Ahmed <touseefahmed1214@gmail.com>
2023-06-09 15:53:00 +0100

    work page responsive

--- assets/index.js
+++ assets/index.js
@@ -186,3 +186,22 @@ inputs.forEach(function(input) {
     this.classList.remove("selected");
   });
 });
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
